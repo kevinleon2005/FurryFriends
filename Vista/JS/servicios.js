355c0bca820b526466c861ebaa27bc9368c8c3ec
Kevin <hernandezkaleth647@gmail.com>
2024-11-26 22:10:58 -0500

    Puse validación también en el registro de los clientes, añadí HTML a medias de los servicios

---
+++ Vista/JS/servicios.js
@@ -0,0 +1,20 @@
+// Esta es la función que se ejecuta cuando el mapa se carga
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 11,
+      center: {
+        lat: 40.645037,
+        lng: -73.880224
+      },
+    });
+
+    var image = 'images/maps-and-flags.png';
+    var beachMarker = new google.maps.Marker({
+      position: {
+        lat: 40.645037,
+        lng: -73.880224
+      },
+      map: map,
+      icon: image
+    });
+}
